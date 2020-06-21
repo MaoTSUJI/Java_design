b4a348435dd067d9077187c4f1e8b9b3c550dbf9
辻真緒 <>
2020-06-21 23:19:54 +0900

    study Factory

---
+++ chap02/Factory/framework/Product.java
@@ -0,0 +1,8 @@
+package framework;
+
+public abstract class Product {
+    public abstract void use();
+}
+// Javaではabstractなコンストラクタを作ることができない
+// Javaではコンストラクタは継承されないので、abstractなコンストラクタには意味がない
+// コンストラクタで製品に名前をつけるのではなく、名前をつけるためのメソッドを別途宣言する必要がある。
