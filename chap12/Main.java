a817160849868468f406b0f98fb59429f653f87a
Mao TSUJI <tsuji.mao36@gmail.com>
2020-10-04 20:06:56 +0900

    practice 12-2

--- chap12/Main.java
+++ chap12/Main.java
@@ -1,11 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    Display b1 = new StringDisplay("Hello, world");
-    Display b2 = new UpdownBorder(b1, '=');
+    // Display b1 = new StringDisplay("Hello, world");
+    MultiStringDisplay md = new MultiStringDisplay();
+    md.add("Good morning!");
+    md.add("Hello!");
+    md.add("Good night!");
+    md.show();
+    Display b2 = new UpdownBorder(md, '=');
+    b2.show();
     Display b3 = new SideBorder(b2, '#');
     // Display b3 = new FullBorder(b2);
-    b1.show();
-    b2.show();
     b3.show();
     // Display b4 = new SideBorder(new FullBorder(new SideBorder(new FullBorder(new
     // StringDisplay("Hello, word")), '*')),
---
+++ chap12/MultiStringDisplay.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiStringDisplay extends Display {
+  private List<String> strArr = new ArrayList<>();
+  private int columns = 0;
+
+  public int getColumns() {
+    return columns;
+  }
+
+  public int getRows() {
+    return strArr.size();
+  }
+
+  public String getRowText(int row) {
+    if (row < getRows()) {
+      return strArr.get(row); // 行の内容
+    } else {
+      return null;
+    }
+  }
+
+  public void add(String string) {
+    strArr.add(string);
+    updateColumns(string);
+  }
+
+  private void updateColumns(String string) { // 文字数を更新
+    if (columns < string.getBytes().length) {
+      columns = string.getBytes().length;
+    }
+    for (int row = 0; row < strArr.size(); row++) {
+      int fills = columns - strArr.get(row).getBytes().length;
+      if (fills > 0) {
+        strArr.set(row, strArr.get(row) + spaces(fills));
+      }
+    }
+  }
+
+  private String spaces(int count) {
+    StringBuffer buf = new StringBuffer();
+    for (int i = 0; i < count; i++) {
+      buf.append(' ');
+    }
+    return buf.toString();
+  }
+
+}
