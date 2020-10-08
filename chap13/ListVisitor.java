2055fb32781157dc03119ec700a140c8dc5c2eb3
Mao TSUJI <tsuji.mao36@gmail.com>
2020-10-08 09:37:57 +0900

    create ListVisitor class

---
+++ chap13/ListVisitor.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+
+public class ListVisitor extends Visitor {
+  private String currentdir = "";
+
+  public void visit(File file) { // Fileクラスのインスタンスに対して行うべき処理
+    System.out.println(currentdir + "/" + file);
+  }
+
+  public void visit(Directory dir) { // Direcroryクラスのインスタンスに対して行うべき処理
+    System.out.println(currentdir + "/" + dir);
+    String savedir = currentdir;
+    currentdir = currentdir + "/" + dir.getName();
+    Iterator it = dir.iterator();
+    while (it.hasNext()) { // ディレクトリの中にあるエントリを1つ1つ訪問していく
+      Entry entry = (Entry) it.next();
+      entry.accept(this); // visitメソッドとacceptメソッドが互いに相手を呼び出している
+    }
+    currentdir = savedir;
+  }
+}
