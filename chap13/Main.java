2548d69b377a2f891dbba18e6a94edda8221489f
Mao TSUJI <tsuji.mao36@gmail.com>
2020-10-15 09:14:34 +0900

    create Trouble class

--- chap13/Main.java
+++ chap13/Main.java
@@ -1,46 +1,58 @@
 package chap13;
 
-import java.util.Iterator;
-
 public class Main {
   public static void main(String[] args) {
     try {
-      System.out.println("Making root entries...");
-      Directory rootdir = new Directory("root");
-      Directory bindir = new Directory("bin");
-      Directory tmpdir = new Directory("tmp");
-      Directory usrdir = new Directory("usr");
-      rootdir.add(bindir);
-      rootdir.add(tmpdir);
-      rootdir.add(usrdir);
-      bindir.add(new File("vi", 10000));
-      bindir.add(new File("latex", 20000));
-      rootdir.accept(new ListVisitor());
-
-      System.out.println("");
-      System.out.println("Making user entries...");
-      Directory yuki = new Directory("yuki");
-      Directory hanako = new Directory("hanako");
-      Directory tomura = new Directory("tomura");
-      usrdir.add(yuki);
-      usrdir.add(hanako);
-      usrdir.add(tomura);
-      yuki.add(new File("diary.html", 100));
-      yuki.add(new File("Composite.java", 200));
-      hanako.add(new File("memo.tex", 300));
-      hanako.add(new File("index.html", 350));
-      tomura.add(new File("game.doc", 400));
-      tomura.add(new File("junk.mail", 500));
+      // System.out.println("Making root entries...");
+      // Directory rootdir = new Directory("root");
+      // Directory bindir = new Directory("bin");
+      // Directory tmpdir = new Directory("tmp");
+      // Directory usrdir = new Directory("usr");
+      // rootdir.add(bindir);
+      // rootdir.add(tmpdir);
+      // rootdir.add(usrdir);
+      // bindir.add(new File("vi", 10000));
+      // bindir.add(new File("latex", 20000));
       // rootdir.accept(new ListVisitor());
 
-      FileFindVisitor ffv = new FileFindVisitor(".html");
-      rootdir.accept(ffv);
-      System.out.println("HTML files are: ");
-      Iterator it = ffv.getFoundFile();
-      while (it.hasNext()) {
-        File file = (File) it.next();
-        System.out.println(file.toString());
-      }
+      // System.out.println("");
+      // System.out.println("Making user entries...");
+      // Directory yuki = new Directory("yuki");
+      // Directory hanako = new Directory("hanako");
+      // Directory tomura = new Directory("tomura");
+      // usrdir.add(yuki);
+      // usrdir.add(hanako);
+      // usrdir.add(tomura);
+      // yuki.add(new File("diary.html", 100));
+      // yuki.add(new File("Composite.java", 200));
+      // hanako.add(new File("memo.tex", 300));
+      // hanako.add(new File("index.html", 350));
+      // tomura.add(new File("game.doc", 400));
+      // tomura.add(new File("junk.mail", 500));
+      // // rootdir.accept(new ListVisitor());
+
+      // FileFindVisitor ffv = new FileFindVisitor(".html");
+      // rootdir.accept(ffv);
+      // System.out.println("HTML files are: ");
+      // Iterator it = ffv.getFoundFile();
+      // while (it.hasNext()) {
+      // File file = (File) it.next();
+      // System.out.println(file.toString());
+      // }
+
+      Directory root1 = new Directory("root1");
+      root1.add(new File("diary.html", 10));
+      root1.add(new File("index.html", 20));
+
+      Directory root2 = new Directory("root2");
+      root2.add(new File("diary.html", 1000));
+      root2.add(new File("index.html", 2000));
+      ElementArrayList list = new ElementArrayList();
+      list.add(root1);
+      list.add(root2);
+      list.add(new File("etc.html", 1234));
+
+      list.accept(new ListVisitor());
 
     } catch (FileTreatmentException e) {
       e.printStackTrace();
