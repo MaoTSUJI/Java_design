712997e1a49fe1fe790ea378e62b560db41364ba
辻真緒 <>
2020-10-23 09:10:32 +0900

    create Colleague interface

---
+++ chap16/Colleague.java
@@ -0,0 +1,5 @@
+public interface Colleague {
+  public abstract void setMediator(Mediator mediator); // 最初に呼び出すメソッド // 相談役が誰かを認識させる
+
+  public abstract void setColleagueEnabled(boolean enabled); // 相談役からくる指示 // 有効状態にするか否か
+}
